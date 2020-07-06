18f256ef8959ff7adb8a8362e0f50c1ba2125bf0
kkyuri0419 <kkyuri0419@gmail.com>
2020-07-06 16:30:07 +0800

    gauge & lux added

--- app/src/main/java/com/example/iot2temi/MainActivity.java
+++ app/src/main/java/com/example/iot2temi/MainActivity.java
@@ -34,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
     TextView txtview;
     TextView tempShowtxt;
     TextView humShowtxt;
+    TextView luxShowtxt;
     CustomGauge gauge1;
     CustomGauge gauge2;
     CustomGauge gauge3;
@@ -43,10 +44,13 @@ public class MainActivity extends AppCompatActivity {
 
     int lasttemp;
     int lasthum;
+    int lastlux;
     int inttemp;
     int inthum;
+    int intlux;
     int difftemp=0;
     int diffhum=0;
+    int difflux=0;
 
 //    Button btnStart;
 //    Button btnStop;
@@ -72,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
         txtview = findViewById(R.id.txtview);
         tempShowtxt = findViewById(R.id.tempShowtxt);
         humShowtxt = findViewById(R.id.humShowtxt);
+        luxShowtxt = findViewById(R.id.luxShowtxt);
         gauge1 = findViewById(R.id.gauge1);
         gauge2 = findViewById(R.id.gauge2);
         gauge3 = findViewById(R.id.gauge3);
@@ -118,27 +123,34 @@ public class MainActivity extends AppCompatActivity {
 
                             Log.e(this.getClass().getName(),msg + "TTTTTTTTTTTTTTTTT");
 
-                            int i = msg.indexOf("t");
+                            int i = msg.indexOf("p");
                             int y = msg.indexOf("y");
-                            String tempval = msg.substring(i+13,i+18);
-                            String humval = msg.substring(y+3,y+8);
+                            int x = msg.indexOf("x");
+                            String tempval = msg.substring(i+4,i+9);
+                            String humval = msg.substring(y+4,y+6);
+                            String luxval = msg.substring(x+4,x+6);
 
                             Log.e(this.getClass().getName(),tempval + "QQQQQQQQQQQQQQQ");
                             Log.e(this.getClass().getName(),humval + "RRRRRRRRRRRRRR");
+                            Log.e(this.getClass().getName(),luxval + "CCCCCCCCCCCCCC");
 
                             tempShowtxt.setText(tempval);
                             humShowtxt.setText(humval);
+                            luxShowtxt.setText(luxval);
 
                             inttemp = Double.valueOf(tempval).intValue();
                             inthum = (Double.valueOf(humval).intValue());
+                            intlux = (Double.valueOf(luxval).intValue());
 
                             if (isFistVal){
                                 gauge1.setValue(inttemp);
                                 gauge2.setValue(inthum);
+                                gauge3.setValue(intlux);
                                 Log.e(this.getClass().getName(),"true");
                                 isFistVal = false;
                                 lasthum = inthum;
                                 lasttemp = inttemp;
+                                lastlux = intlux;
                             }else{
                                 difftemp = inttemp - lasttemp;
                                 Log.e(this.getClass().getName(), "lasttemp : " +(String.valueOf(lasttemp)));
@@ -146,6 +158,23 @@ public class MainActivity extends AppCompatActivity {
                                 diffhum = inthum - lasthum;
                                 Log.e(this.getClass().getName(), "lasthum : " +(String.valueOf(lasthum)));
                                 Log.e(this.getClass().getName(),"diffhum : " +(String.valueOf(diffhum)));
+                                difflux = intlux - lastlux;
+                                Log.e(this.getClass().getName(), "lastlux : " +(String.valueOf(lastlux)));
+                                Log.e(this.getClass().getName(),"difflux : " +(String.valueOf(difflux)));
+
+                                if (difflux < 0){
+                                    difflux *= -1;
+                                    Runnable LuxDrunnable = new LuxDecRunnable(inthum, lasthum, diffhum);
+                                    new Thread(LuxDrunnable).start();
+
+                                }else if (difflux > 0){
+                                    Runnable LuxIncRunnable = new LuxIncRunnable(inttemp, lasttemp, difftemp);
+                                    new Thread(LuxIncRunnable).start();
+
+                                }else if (difflux == 0){
+                                    lastlux = intlux;
+                                    Log.e(this.getClass().getName(),"SAME LUX");
+                                }
 
                                 if (difftemp < 0){
                                     difftemp *= -1;
@@ -158,6 +187,7 @@ public class MainActivity extends AppCompatActivity {
 
                                 }else if (difftemp == 0){
                                     lasttemp = inttemp;
+                                    Log.e(this.getClass().getName(),"SAME TEMP");
                                 }
 
                                 if (diffhum < 0){
@@ -165,16 +195,13 @@ public class MainActivity extends AppCompatActivity {
                                     Runnable humDrunnable = new HumDecRunnable(inthum, lasthum, diffhum);
                                     new Thread(humDrunnable).start();
 
-                                }else if (difftemp > 0){
+                                }else if (diffhum > 0){
+                                    Runnable HumIncRunnable = new HumIncRunnable(inttemp, lasttemp, difftemp);
+                                    new Thread(HumIncRunnable).start();
 
-                                    Runnable humIrunnable = new HumIncRunnable(inttemp, lasttemp, difftemp);
-                                    new Thread(humIrunnable).start();
-                                    try{Thread.sleep(500);
-                                    }catch (InterruptedException e) {
-                                        e.printStackTrace();
-                                    }
                                 }else if (diffhum == 0){
                                     lasthum = inthum;
+                                    Log.e(this.getClass().getName(),"SAME HUM");
                                 }
                             }
 
@@ -233,14 +260,9 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
 
             for (int i = 1; i <= difftemp; i++){
-                if (lasttemp < 20){
-                    lasttemp = 20;
-                }else if(lasttemp >40){
-                    lasttemp = 40;
-                }
                 gauge1.setValue(lasttemp+=1);
                 try {
-                    Thread.sleep(50);
+                    Thread.sleep(30);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -259,14 +281,9 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
 
             for (int i = 1; i <= difftemp; i++){
-                if (lasttemp < 20){
-                    lasttemp = 20;
-                }else if(lasttemp >40){
-                    lasttemp = 40;
-                }
                 gauge1.setValue(lasttemp-=1);
                 try {
-                    Thread.sleep(50);
+                    Thread.sleep(30);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -284,21 +301,17 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void run() {
 
-            for (int i = 1; i <= difftemp; i++) {
-                if (lasthum < 30){
-                    lasthum = 30;
-                }else if(lasthum >50){
-                    lasthum = 50;
-                }
+            for (int i = 1; i <= diffhum; i++) {
                 gauge2.setValue(lasthum+=1);
                 try {
-                    Thread.sleep(50);
+                    Thread.sleep(30);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
 //                Log.e(this.getClass().getName(),"Thread Success33333");
+                lasthum = inthum;
             }
-            lasthum = inthum;
+
         }
     }
 
@@ -311,20 +324,58 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
 
             for (int i = 1; i <= diffhum; i++){
-                if (lasthum < 30){
-                    lasthum = 30;
-                }else if(lasthum >50){
-                    lasthum = 50;
-                }
                 gauge2.setValue(lasthum-=1);
                 try {
-                    Thread.sleep(50);
+                    Thread.sleep(30);
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
 //                Log.e(this.getClass().getName(),"Thread Success444444");
             }
             lasthum = inthum;
+        }
+    }
+
+    public class LuxIncRunnable implements Runnable {
+
+        public LuxIncRunnable(int pres, int last, int diff){
+
+        }
+        @Override
+        public void run() {
+
+            for (int i = 1; i <= difflux; i++) {
+                gauge3.setValue(lastlux+=1);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+//                Log.e(this.getClass().getName(),"Thread Success33333");
+                lastlux = intlux;
+            }
+
+        }
+    }
+
+    public class LuxDecRunnable implements Runnable {
+
+        public LuxDecRunnable(int pres, int last, int diff){
+
+        }
+        @Override
+        public void run() {
+
+            for (int i = 1; i <= difflux; i++){
+                gauge3.setValue(lastlux-=1);
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+//                Log.e(this.getClass().getName(),"Thread Success444444");
+            }
+            lastlux = intlux;
         }
     }
 }
